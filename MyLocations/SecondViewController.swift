6ce5cafcca02edea7e5154db9d305293589afb10
aurelijuslape <aurelijus_lape@yahoo.com>
2019-06-18 21:12:38 +0300

    Initial Commit

---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Aurelijus Lape on 18/06/2019.
+//  Copyright © 2019 Aurelijus Lape. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
